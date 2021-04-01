1b1f3fc2182f90820b1eb24bbabbfd3f0727110e
edgarsamp <edgar-sampaio@hotmail.com>
2021-04-01 07:25:11 -0300

    Iniciando a classse Problem

--- src/classroom/Classroom.java
+++ src/classroom/Classroom.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package classroom;
 
-/**
- *
- * @author antonio_vmoura
- */
 public class Classroom {
+    private int id;
+    private String theme, description;
+    
     
 }
--- src/codifico/Codifico.java
+++ src/codifico/Codifico.java
@@ -1,21 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package codifico;
 
-/**
- *
- * @author antonio_vmoura
- */
 public class Codifico {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
 }
